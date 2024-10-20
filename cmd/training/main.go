9ddce4a472b77295bdf8b633303ffd60e23866a5
YusukeTakatsu <github ys.taka44@gmail.com>
2024-10-20 10:05:53 +0900

    feat:setting-db-connection

--- cmd/training/main.go
+++ cmd/training/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+	"github.com/yusuke-takatsu/go-training/config/database"
+	"log"
+)
+
 func main() {
+	loadEnv()
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
